f184d19846ddf008ddae82b742bc8c2a2fe93996
Jason Stillwell <jstillwell@zendesk.com>
2013-02-04 13:45:55 -0800

    both master and slave should be reachable from the hosts OS. to allow for tests to drive both database.

--- roles/slave.rb
+++ roles/slave.rb
@@ -4,6 +4,7 @@ description "slave database role"
 run_list "recipe[apt]", "recipe[build-essential]", "recipe[mysql::server]", "recipe[mysql::ruby]", "recipe[zendesk::slave]"
 
 override_attributes(:mysql => {
+    :"bind_address" => "0.0.0.0"
   },
   :build_essential => {
     :compiletime => true
